671c4e8d8060bf45efb6ed8cb33aafa7be362674
rachel-brown2 <47750667+rachel-brown2@users.noreply.github.com>
2020-12-29 20:26:28 +0000

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 21:02:47 2020
+
+@author: rache
+"""
+from skimage.feature import hog
+import matplotlib.image as mpimg
+from scipy.ndimage.measurements import label
+import numpy as np
+import cv2
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+     # Call with two outputs if vis==True
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, 
+                                  pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), 
+                                  transform_sqrt=False, 
+                                  visualize=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, 
+                       pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), 
+                       transform_sqrt=False, 
+                       visualize=vis, feature_vector=feature_vec)
+        return features
+
+def extract_features(imgs, cspace='RGB', orient=9, 
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for i in imgs:
+        # Read in each one by one
+        image = mpimg.imread(i)
+        # apply color conversion if other than 'RGB'
+        if cspace != 'RGB':
+            if cspace == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif cspace == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif cspace == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif cspace == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif cspace == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else: feature_image = np.copy(image)
+
+        # Call get_hog_features() with vis=False, feature_vec=True
+        if hog_channel == 'ALL':
+            hog_features = []
+            for channel in range(feature_image.shape[2]):
+                hog_features.append(get_hog_features(feature_image[:,:,channel], 
+                                    orient, pix_per_cell, cell_per_block, 
+                                    vis=False, feature_vec=True))
+            hog_features = np.ravel(hog_features)        
+        else:
+            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
+                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+        # Append the new feature vector to the features list
+        features.append(hog_features)
+    # Return list of feature vectors
+    return features
+
+def find_cars(img, ystart, ystop, scale, cspace, hog_channel, svc, X_scaler, orient, 
+              pix_per_cell, cell_per_block, spatial_size, hist_bins, show_all_rectangles=False):
+    
+    # Define array of rectangles surrounding cars that were detected
+    rectangles = []
+    
+    # Normalize image
+    img = img.astype(np.float32)/255
+    search_img = img[ystart:ystop,:,:]
+
+    # Apply color conversion if other than 'RGB'
+    if cspace != 'RGB':
+        if cspace == 'HSV':
+            search_ctrans = cv2.cvtColor(search_img, cv2.COLOR_RGB2HSV)
+        elif cspace == 'LUV':
+            search_ctrans = cv2.cvtColor(search_img, cv2.COLOR_RGB2LUV)
+        elif cspace == 'HLS':
+            search_ctrans = cv2.cvtColor(search_img, cv2.COLOR_RGB2HLS)
+        elif cspace == 'YUV':
+            search_ctrans = cv2.cvtColor(search_img, cv2.COLOR_RGB2YUV)
+        elif cspace == 'YCrCb':
+            search_ctrans = cv2.cvtColor(search_img, cv2.COLOR_RGB2YCrCb)
+    else: search_ctrans = np.copy(image)   
+    
+    # Rescale image if not 1.0
+    if scale != 1:
+        img_shape = search_ctrans.shape
+        search_ctrans = cv2.resize(search_ctrans, (np.int(img_shape[1]/scale), np.int(img_shape[0]/scale)))
+    
+    # Select color channel for HOG 
+    if hog_channel == 'ALL':
+        channel_1 = search_ctrans[:,:,0]
+        channel_2 = search_ctrans[:,:,1]
+        channel_3 = search_ctrans[:,:,2]
+    else: 
+        channel_1 = ctrans_tosearch[:,:,hog_channel]
+
+    # Define blocks
+    nx_blocks = (channel_1.shape[1] // pix_per_cell)+1  #-1
+    ny_blocks = (channel_1.shape[0] // pix_per_cell)+1  #-1 
+    
+    # Define sampling rate with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell)-1 
+    cells_per_step = 2
+    nx_steps = (nx_blocks - nblocks_per_window) // cells_per_step
+    ny_steps = (ny_blocks - nblocks_per_window) // cells_per_step
+    
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(channel_1, orient, pix_per_cell, cell_per_block, feature_vec=False)   
+    if hog_channel == 'ALL':
+        hog2 = get_hog_features(channel_2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+        hog3 = get_hog_features(channel_3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    
+    for x in range(nx_steps):
+        for y in range(ny_steps):
+            y_position = y * cells_per_step
+            x_position = x * cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[y_position:y_position+nblocks_per_window, x_position:x_position+nblocks_per_window].ravel()
+            if hog_channel == 'ALL':
+                hog_feat2 = hog2[y_position:y_position+nblocks_per_window, x_position:x_position+nblocks_per_window].ravel() 
+                hog_feat3 = hog3[y_position:y_position+nblocks_per_window, x_position:x_position+nblocks_per_window].ravel() 
+                hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+            else:
+                hog_features = hog_feat1
+
+            x_left = x_position*pix_per_cell
+            y_top = y_position*pix_per_cell
+            
+            test_prediction = svc.predict(hog_features.reshape(-1,1188))
+            
+            if test_prediction == 1 or show_all_rectangles:
+                x_box_left = np.int(x_left*scale)
+                y_top_draw = np.int(y_top*scale)
+                window_draw = np.int(window*scale)
+                rectangles.append(((x_box_left, y_top_draw+ystart),(x_box_left+window_draw,y_top_draw+window_draw+ystart)))
+                
+    return rectangles
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    random_color = False
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        if color == 'random' or random_color:
+            color = (np.random.randint(0,255), np.random.randint(0,255), np.random.randint(0,255))
+            random_color = True
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    rects = []
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        rects.append(bbox)
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image and final rectangles
+    return img, rects
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def process_frame(img, svc):
+
+    # Create array to hold the select boxes where we found cars
+    rectangles = []
+
+    # Set parameters for find_cars() function
+    colorspace = 'YUV'
+    orient = 11
+    pix_per_cell = 16
+    cell_per_block = 2
+    hog_channel = 'ALL'
+
+    # Set dimentions for rectangle layer
+    ystart = 400
+    ystop = 464
+    scale = 1.0
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 416
+    ystop = 480
+    scale = 1.0
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 400
+    ystop = 496
+    scale = 1.5
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 432
+    ystop = 528
+    scale = 1.5
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 400
+    ystop = 528
+    scale = 2.0
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 432
+    ystop = 560
+    scale = 2.0
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+    # Set dimentions for rectangle layer
+    ystart = 400
+    ystop = 596
+    scale = 3.5
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+    
+  # Set dimentions for rectangle layer
+    ystart = 464
+    ystop = 660
+    scale = 3.5
+    rectangles.append(find_cars(img, ystart, ystop, scale, colorspace, hog_channel, svc, None, 
+                           orient, pix_per_cell, cell_per_block, None, None))
+
+
+    # Flatten list of lists
+    rectangles = [item for sublist in rectangles for item in sublist] 
+    
+    # Get heatmap image and apply threshold to frame
+    heatmap_img = np.zeros_like(img[:,:,0])
+    heatmap_img = add_heat(heatmap_img, rectangles)
+    heatmap_img = apply_threshold(heatmap_img, 1)
+    
+    # Get labels from heatmap output
+    labels = label(heatmap_img)
+    
+    # Draw boxes on image based on above calculations
+    draw_img, rects = draw_labeled_bboxes(np.copy(img), labels)
+    
+    return draw_img, rects
+
+def londonWeather():
+    w = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=bfb86295e5a377499d0da0ad5dd50381')
+    Weatherdata = w.json()
+    #print(Weatherdata)
+    LondonTempC = float(Weatherdata['main']['temp'])- 273.15
+    # LondonTempC = float(LondonTempK )
+    #print('{:.2f}'.format(LondonTempC))
+    LondonWeatherDescription = Weatherdata ['weather'][0]['description']
+    return LondonTempC, LondonWeatherDescription
+    
+
+def updateSheets(timenow, LondonTempC, LondonWeatherDescription, service, spreadsheetID, cell_range_insert):
+    Bodyinfo = ((timenow.strftime("%d/%m/%Y"), timenow.strftime("%H:%M:%S"),LondonWeatherDescription,'{:.2f}'.format(LondonTempC)),('','','',''))
+    value_range_body = {
+        'majorDimension': 'ROWS',
+        'values': Bodyinfo}
+    request=service.spreadsheets().values().append(spreadsheetId=spreadsheetID, range=cell_range_insert, valueInputOption='USER_ENTERED', body=value_range_body)
+    request.execute()
