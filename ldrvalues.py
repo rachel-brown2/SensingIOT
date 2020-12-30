6cccee057012f9d962154127e68657c45bb46afc
rachel-brown2 <47750667+rachel-brown2@users.noreply.github.com>
2020-12-30 19:09:28 +0000

    Update ldrvalues.py

--- ldrvalues.py
+++ ldrvalues.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 30 12:47:26 2020
-
-@author: rache
-"""
-
 ###need to figure out how to do a low pass filter####
 
 import time
@@ -16,11 +9,11 @@ import Adafruit_MCP3008
 SPI_PORT   = 0 # the port the LDR is connected to 
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-LDR_value = mcp.read_adc(0)
+LDR_value=[0]
 
 while True:
-    LDR_value[0] = mcp.read_adc(0)
-    if (LDR_value >1200):
+    LDR_value = mcp.read_adc(0)
+    if (LDR_value >1):
         print('| {0:>4} |'.format(LDR_value))
     else:
         print('too small wahhh')
