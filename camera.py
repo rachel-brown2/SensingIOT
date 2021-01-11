2e46a9375b2a837cd346e0b58b81c0e9175e207c
rachel-brown2 <47750667+rachel-brown2@users.noreply.github.com>
2021-01-11 10:58:04 +0000

    Update and rename cameracode.py to camera.py

--- camera.py
+++ camera.py
@@ -1,23 +1,17 @@
-# -*- cod--ing: utf-8 -*-
-"""
-Created on Thu Dec 24 16:36:15 2020
-
-@author: rache
-"""
-
 from picamera import PiCamera
 from time import sleep
 import time
 
 camera = PiCamera()
 
-timeout = time.time() + 60  #running for 60 seconds
+timeout = time.time() + 60*60*3  #running for 3 hours
 
 i=1        
 
 camera.start_preview()
+camera.resolution = (1280, 720)
 while (time.time()<timeout):
    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
    i=i+1
-   sleep(5)
+   sleep(2)
 camera.stop_preview()
